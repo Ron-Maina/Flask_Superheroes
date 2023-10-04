c0a5a769c0191250f77b064f95d847c812ec66bc
Ron Maina <ronmwangi00@gmail.com>
2023-10-04 12:58:12 +0300

    Static routes

--- app/app.py
+++ app/app.py
@@ -10,7 +10,12 @@ from models import db, Heroes, Powers, HeroPowers
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='../client/build',
+    template_folder='../client/build'
+)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
